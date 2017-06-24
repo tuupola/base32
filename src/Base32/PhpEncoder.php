c4d8ed837f98677120b1b5d417639594d4cf6eac
Mika Tuupola <tuupola@appelsiini.net>
2017-06-24 15:29:20 +0700

    First working PHP and GMP encoders

---
+++ src/Base32/PhpEncoder.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the Base32 package
+ *
+ * Copyright (c) 2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/base32
+ *
+ */
+
+namespace Tuupola\Base32;
+
+use Tuupola\Base32;
+
+class PhpEncoder
+{
+    private $options = [
+        "characters" => Base32::RFC4648,
+        "padding" => "=",
+    ];
+
+    public function __construct($options = [])
+    {
+        $this->options = array_merge($this->options, (array) $options);
+    }
+
+    public function encode($data)
+    {
+        $data = trim($data);
+        if (empty($data)) {
+            return "";
+        }
+
+        /* Split to characters. */
+        $data = str_split($data);
+        $binary = implode("", array_map(function ($character) {
+            return sprintf("%08b", ord($character));
+        }, $data));
+
+        /* Split to five bit chunks and make sure last chunk has five bits. */
+        $binary = str_split($binary, 5);
+        $last = array_pop($binary);
+        $binary[] = str_pad($last, 5, "0", STR_PAD_RIGHT);
+
+        /* Convert each five bits to Base32 character. */
+        $encoded = implode("", array_map(function ($fivebits) {
+            $index = bindec($fivebits);
+            return $this->options["characters"][$index];
+        }, $binary));
+
+        /* Pad to eight characters when requested. */
+        if (!empty($this->options["padding"])) {
+            if ($modulus = strlen($encoded) % 8) {
+                $padding = 8 - $modulus;
+                $encoded .= str_repeat($this->options["padding"], $padding);
+            }
+        }
+
+        return $encoded;
+    }
+
+    public function decode($data)
+    {
+        $data = trim($data);
+        if (empty($data)) {
+            return "";
+        }
+
+        $data = str_split($data);
+        $data = array_map(function ($character) {
+            if ($character !== $this->options["padding"]) {
+                $index = strpos($this->options["characters"], $character);
+                return sprintf("%05b", $index);
+            }
+        }, $data);
+        $binary = implode("", $data);
+
+        /* Split to eight bit chunks. */
+        $data = str_split($binary, 8);
+
+        /* Make sure binary is divisible by eight by ignoring the incomplete byte. */
+        $last = array_pop($data);
+        if (8 === strlen($last)) {
+            $data[] = $last;
+        }
+
+        return implode("", array_map(function ($byte) {
+            return chr(bindec($byte));
+        }, $data));
+    }
+}
